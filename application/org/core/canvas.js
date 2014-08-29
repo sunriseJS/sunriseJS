bc1c6b6ee1a44c1b4ebfb59586885ea28e09631f
Jonathan Wiemers <jwiemers@webtwoart.com>
2014-08-30 00:37:37 +0300

    licence

--- application/org/core/canvas.js
+++ application/org/core/canvas.js
@@ -1,6 +1,24 @@
+/**
+ * @license sunriseJS Game Engine
+ * @copyright (C) 2014 - 2014 Jonas Gerdes, Jonathan Wiemers
+ * http://www.sunrisejs.net
+ *
+ * sunriseJS is licensed under the MIT License.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ *
+ * 
+ * @param  {[type]} sr
+ * @return {[type]}
+ */
 (function(sr){	
 	var _private = sr._private;
 
+	/**
+	 * Initialising the canvas as a child of the sunriseJS-app element 
+	 * with the data the user specified in the config area.
+	 * @return {[type]}
+	 */
 	sr.initCanvas = function(){
 		screenWidth = game.config.screenWidth;
 		screenHeight = game.config.screenHeight;
--- application/org/core/sunrise.js
+++ application/org/core/sunrise.js
@@ -1,8 +1,18 @@
+/**
+ * @license sunriseJS Game Engine
+ * @copyright (C) 2014 - 2014 Jonas Gerdes, Jonathan Wiemers
+ * http://www.sunrisejs.net
+ *
+ * sunriseJS is licensed under the MIT License.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ *
+ * 
+ * @param  {[type]} sr
+ * @return {[type]}
+ */
 (function(sr){
 	
-
-	
-
 	var _private = sr._private = sr._private || {};
 	_private.data = {};
 
