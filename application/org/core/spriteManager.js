f4ebdd8a04b33d8f316806e0daff4225dc623cea
JGerdes <jonas.gerdes@gmx.net>
2014-08-30 00:46:56 +0300

    Added Sprite to SpriteManager

--- application/org/core/spriteManager.js
+++ application/org/core/spriteManager.js
@@ -1,7 +1,64 @@
 (function(sr) {
-	_private = sr._private;
+	var _private = sr._private;
 
-	_private.spritePool = []; //container for all sprites
+	var spritePool = {}; //container for all sprites
+
+
+	//Classes
+
+	sr.Sprite = function(image, width, height, animations){
+		this.image = image;
+		this.width = width;
+		this.height = height;
+		this.animations = animations;
+
+		//Context from other modules
+		this.context = _private.data.canvas.context;
+
+		//Until an actual animation is set, use first one
+		for(anim in animations){
+			this.currentAnimation = anim;
+			break;
+		}
+
+		this.currentFrame = 0;
+		this.lastDrawTime = Date.now();
+		this.frameDuration = 60; //in Milliseconds
+
+		//Calculate values for spritesheet
+
+		this.cols = Math.floor( image.width / width );
+		this.rows = Math.floor( image.height / height );
+
+	}
+
+	/**
+	 * Draws itselfs at the specified position.
+	 * @param x Horizontal position
+	 * @param y Vertical position
+	 * @param w Width of drawn image. Useful for stretching. Optional.
+	 * @param h Height of drawn image. Useful for stretching. Optional.
+	 */
+	sr.Sprite.prototype.draw = function(x, y, w, h){
+		var width = w || this.width;
+		var height = h || this.height;
+		var frame = this.currentAnimation[this.currentFrame];
+
+		var sourceX = frame % this.cols;
+		var sourceY = Math.floor(frame / this.rows);
+		drawImage(this.image, sourceX, sourceY, this.width, this.height, 
+					x, y, width, height);
+
+		var now = Date.now();
+		var delta = now - this.lastDrawTime;
+		if(delta >= this.frameDuration){
+			this.currentFrame++;
+			if(this.currentFrame >= this.currentAnimation.length){
+				this.currentFrame = 0;
+			}
+		}
+
+	}
 
 	/**
 	 * Loads an imagefile an creates a sprite from it.
@@ -26,6 +83,8 @@
 	 * @param  
 	 * @return void
 	 */
+	
+	//Todo: Think of some way to know, when Sprite is ready. Callback?
 	sr.createSprite = function(data) {
 
 		//Test whether all required parameters are given
@@ -34,11 +93,37 @@
 			requiredParams.push('tileWidth');
 			requiredParams.push('tileHeight');
 		}
-		for(param in requiredParams){
+		requiredParams.forEach(function(param){
 			if(data[param] === undefined){
 				throw new Error('No element "'+param+'" specified in data');
 			}
+		});
+
+		
+
+		var image = new Image();
+		image.onload = function() {
+			if(data.tileWidth === undefined){
+				data.tileWidth = this.width;
+				data.tileHeight = this.height;
+			}
+			//Make sure there is at least one animation
+			if(data.animation.default !== undefined){
+				data.animations.default = [0];
+			}
+
+			//Name already used? (Has to be checked here, otherwise some other sprite
+			//	with an image, which loads faster, could already has the name
+			if(spritePool[data.name] !== undefined){
+				throw new Error('Sprite with name '+data.name+' already exists');
+			}else{
+				spritePool[data.name] = new sr.Sprite(this, data.width, data.height, data.animations);
+			}
+
 		}
+		image.src = data.name;
+
+
 
 	}
 	
