a7f50f211980a1717878a6836098e33a37d63aab
JGerdes <jonas.gerdes@gmx.net>
2014-08-31 13:28:25 +0300

    Created Stage

---
+++ application/org/core/Stage.js
@@ -0,0 +1,31 @@
+/**
+ * @license sunriseJS Game Engine
+ * @copyright (C) 2014 - 2014 Jonas Gerdes, Jonathan Wiemers
+ * http://www.sunrisejs.net
+ *
+ * sunriseJS is licensed under the MIT License.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ *
+ */
+(function($sr){	
+	var $rootScope = $sr.$rootScope;
+	var entities = [];
+
+	$sr.stage = {};
+
+	$sr.stage.add = function(entity){
+		if(!entity instanceof $sr.Entity){
+			throw new Error('Only entities can be added to stage');
+		}
+		entities.push(entity);
+	}
+
+	$rootScope.drawStage = function(){
+		entities.forEach(function(entity){
+			entity.draw();
+		});
+	}
+
+	
+})($sr = window.$sr = window.$sr || {});
