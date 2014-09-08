eeb2d614b1049ac09d9b8da9eb55773c2cad2666
JGerdes <jonas.gerdes@gmx.net>
2014-09-08 23:37:05 +0300

    added some fancy animation for testing purposes

--- game.js
+++ game.js
@@ -6,12 +6,19 @@ var game = {
 		images: {
 			'player' 		: 'assets/graphics/char1.png',
 			'player-anim' 	: {
-								source: 'assets/graphics/spritesheet.png',
+								source: 'assets/graphics/soldier.png',
 								tileWidth: 64,
 								tileHeight: 128,
 								animations: {
-									left: [0,1,2,3,4,5,6,7,8,9,10,11,12],
-									right: [12,11,10,9,8,7,6,5,4,3,2,1,0]
+									'w': [0,1,2,3,4,5,6,7,8,9,10,11],
+									'nw': [12,13,14,15,16,17,18,19,20,21,22,23],
+									'n': [24,25,26,27,28,29,30,31,32,33,34,35],
+									'ne':[36,37,38,39,40,41,42,43,44,45,46,47],
+									'e': [48,49,50,51,52,53,54,55,56,57,58,59],
+									'se': [60,61,62,63,64,65,66,67,68,69,70,71],
+									's' : [72,73,74,75,76,77,78,79,80,81,82,83],
+									'sw' : [84,85,86,87,88,89,90,91,92,93,94,95],
+									'rot': [3,15,28,39,51,63,75,87]
 								}
 
 							},
@@ -52,27 +59,21 @@ var game = {
 	run: function($scope){
 		$scope.fpsdom.innerHTML = $sr.fps.getFps();
 
-		/*if($scope.toLeft){
+		if($scope.toLeft){
 			$scope.player.position.sub(3,0);
 			if($scope.player.position.x <= 0){
 				$scope.toLeft = false;
-				$scope.player.setAnimation('right');
+				$scope.player.setAnimation('e');
 			}
 		}else{
 			$scope.player.position.add(3,0);
 			if($scope.player.position.x >= 1280){
 				$scope.toLeft = true;
-				$scope.player.setAnimation('left');
+				$scope.player.setAnimation('w');
 			}
-		}*/
+		}
 
-		$scope.player.position.add(-1,1);
-		if($scope.player.position.x <= 0){
-			$scope.player.position.x = 1280;
-		}
-		if($scope.player.position.y >= 720){
-			$scope.player.position.y = 0;
-		}
+		
 
 		// $sr.strokeRect($scope.x, $scope.y,50,50);
 	},
