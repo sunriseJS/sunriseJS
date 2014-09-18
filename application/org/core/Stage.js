f39e91190df2582347548138a05a1fcb98bf245f
JGerdes <jonas.gerdes@gmx.net>
2014-09-18 17:50:30 +0300

    Stage is able to draw maps (from spritesheets)

--- application/org/core/Stage.js
+++ application/org/core/Stage.js
@@ -11,6 +11,7 @@
 (function($sr){	
 	var $rootScope = $sr.$rootScope;
 	var entities = [];
+	var mapBuffer;
 
 	$sr.stage = {};
 
@@ -22,7 +23,34 @@
 		console.log('added to stage. entities:',entities);
 	}
 
+	$sr.stage.setMap = function(map){
+		var tilesetInfo = game.config.images[map.tileset],
+			tileset = $rootScope.ressources.images[map.tileset],
+			mapWidth = tilesetInfo.tileWidth*map.width, 
+			mapHeight = tilesetInfo.tileHeight*map.height,
+			tileWidth = tilesetInfo.tileWidth,
+			tileHeight = tilesetInfo.tileHeight,
+			canvas = document.createElement('canvas'),
+			context = canvas.getContext('2d');
+		canvas.width = mapWidth;
+		canvas.height = mapHeight;
+		map.tiles.forEach(function(tile){
+			var setX = tile[2]*tileWidth,
+				setY = tile[3]*tileHeight,
+				mapX = tile[0]*tileWidth,
+				mapY = tile[1]*tileHeight;
+			context.drawImage(tileset,setX, setY, tileWidth, tileHeight,
+									mapX, mapY, tileWidth, tileHeight);
+		});
+		mapBuffer = canvas;
+
+	}
+
 	$rootScope.drawStage = function(){
+		if(mapBuffer !== undefined){
+
+			$rootScope.canvas.context.drawImage(mapBuffer,0,0);
+		}
 		entities.forEach(function(entity){
 			entity.draw();
 		});
