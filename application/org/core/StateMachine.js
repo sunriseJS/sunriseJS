1e716e9a9b9fb2184d821383ff871fb0ba7c36e6
Jonathan Wiemers <jwiemers@webtwoart.com>
2014-09-29 22:02:01 +0300

    changes to StateMachine due to changes to the Entity System in #11

--- application/org/core/StateMachine.js
+++ application/org/core/StateMachine.js
@@ -8,21 +8,7 @@
  *
  */
 
-/**
- * Excample for player
- * 
- * {
- * 		name: "default",!!!!!!!
- * 		speed: 0,
- * 		sound: "none"
- * }
- *
- * 
- */
-
 $sr.StateMachine = (function(){ 
-   
-
     /**
      * construktor
      * @param {[type]} states min 1 = default
@@ -32,10 +18,18 @@ $sr.StateMachine = (function(){
     	this.currentState = 'default';
     };
 
+    /**
+     * returns current state.
+     */
     StateMachine.prototype.getCurrentState = function(){
     	return this.states[this.currentState];
     };
 
+    /**
+     * Sets current state.
+     * Throws error if state is not defined.
+     * @param {[type]} name [the name of the new current state]
+     */
 	StateMachine.prototype.setCurrentState = function(name){
     	if(this.states[name] != undefined){
     		this.currentState = name;
@@ -44,6 +38,12 @@ $sr.StateMachine = (function(){
     	throw 'no State with name: '+name;
     };
 
+    /**
+     * Adds a new state to the states object.
+     * Will throw error if name is already in use
+     * @param {[type]} name  [the name of the new state]
+     * @param {[type]} state [the state object]
+     */
 	StateMachine.prototype.addState = function(name,state){
         if(this.states[name] == undefined){
             this.states[name] = state;
