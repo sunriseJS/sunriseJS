3389e601cb8c05347491e9a8564de155f7cc22a7
JGerdes <jonas.gerdes@gmx.net>
2014-10-06 18:26:37 +0300

    collsionSolver provides additional information to collisions (normal, penetration depth)

--- application/org/core/collisionSolver.js
+++ application/org/core/collisionSolver.js
@@ -8,7 +8,42 @@
 					var colY = (s.y >= f.y && s.y <= f.y + f.height);
 					colY = colY || (f.y >= s.y && f.y <= s.y + s.height);
 					//console.log('testCollision rectangle:',colX, colY, f, s);
-					return colX && colY;
+					if(colX && colY){
+						var collision = {},
+							n = {
+								x: f.x - s.x,
+								y : f.y - s.y
+							},
+							fHalfWidth = f.width/2,
+							sHalfWidth = s.width/2,
+							xOverlap = fHalfWidth + sHalfWidth - Math.abs(n.x);
+
+						if(xOverlap > 0){
+							var fHalfHeight = f.height/2,
+								sHalfHeight = s.height/2
+								yOverlap = fHalfHeight + fHalfHeight - Math.abs(n.y);
+
+							if(yOverlap > 0){
+								if(xOverlap > yOverlap){
+									if(n.x < 0){
+										collision.normal = {x: -1, y: 0};
+									}else{
+										collision.normal = {x: 1, y: 0};
+									}
+									collision.penetration = xOverlap;
+								}else{
+									if(n.y < 0){
+										collision.normal = {x: 0, y: -1};
+									}else{
+										collision.normal = {x: 0, y: 1};
+									}
+									collision.penetration = yOverlap;
+								}
+								return collision;
+							}
+						}
+					}
+					return false;
 				},
 				circle : function(f, s){
 					return false;
@@ -63,9 +98,10 @@
 	    	s.x += o[1].x;
 	    	s.y += o[1].y;
 
-		    if(colliderTesters[c1][c2](f,s)){
-		    	o[0].emit('collision',{other: o[1]});
-		    	o[1].emit('collision',{other: o[0]});
+	    	var collision = colliderTesters[c1][c2](f,s);
+		    if(collision !== false){
+		    	o[0].emit('collision',{other: o[1], collsion: collision});
+		    	o[1].emit('collision',{other: o[0], collision: collision});
 		    }
 		    f.x -= o[0].x;
 	    	f.y -= o[0].y;
