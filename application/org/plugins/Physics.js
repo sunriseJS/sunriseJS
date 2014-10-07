308c8113248a258a91a0a8ad6bd7a4418c161629
JGerdes <jonas.gerdes@gmx.net>
2014-10-07 19:36:35 +0300

    extendend physics

--- application/org/plugins/Physics.js
+++ application/org/plugins/Physics.js
@@ -36,13 +36,14 @@
 		 */
 		Physics = function(config){
 			srfn.Component.call(this);
+			this.data = {};
 			var self = this,
-				speed = new utilfn.Vec2(0,0),
+				speed = this.data.speed = new utilfn.Vec2(0,0),
 				forces = [];
 				unit = 1/60, //pixel/second
-				mass = (config.mass === undefined) ? 1 : config.mass,
-				inertia = (config.inertia === undefined) ? 0.99 : config.inertia;
-
+				mass = this.data.mass = (config.mass === undefined) ? 1 : config.mass,
+				inertia = (config.inertia === undefined) ? 0.99 : config.inertia,
+				restitution = this.data.restitution = 1;
 			config.forces = config.forces || [];
 			for(var i=0; i<config.forces.length; i++){
 				forces.push(new utilfn.Vec2(config.forces[i].x,config.forces[i].y));
@@ -60,10 +61,28 @@
 			});
 
 			this.on('collision', function(data){
-				//self.entity.x -= speed.x;
-				speed.x = 0;
-				//self.entity.y -= speed.y;
-				speed.y = 0;
+				console.log(data);
+				var otherSpeed = data.other.getComponentData('Physics', 'speed'),
+					otherRestitution = data.other.getComponentData('Physics', 'restitution'),
+					otherMass = data.other.getComponentData('Physics', 'mass'),
+					normal = new utilfn.Vec2(data.collision.normal.x, data.collision.normal.y);
+				if(!(otherSpeed && otherRestitution && otherMass)){
+					return;
+				}
+				var	relativeSpeed = utilfn.Vec2.sub(otherSpeed, speed),
+					speedNormal = utilfn.Vec2.dot(relativeSpeed, normal);
+
+				if(speedNormal > 0){
+					return;
+				}
+
+				var bounciness = Math.min(restitution, otherRestitution),
+					force = (-(1+bounciness)*speedNormal)/((1/mass) + (1/otherMass)),
+					impulse = utilfn.Vec2.multiply(normal, force);
+
+				speed.sub(utilfn.Vec2.multiply(impulse, 1/mass));
+				otherSpeed.sub(utilfn.Vec2.multiply(impulse, 1/otherMass));
+
 			});
 
 
