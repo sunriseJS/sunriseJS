ecc721935d259bd42ba5eb374db15eeadcfc3a1b
JGerdes <jonas.gerdes@gmx.net>
2014-10-07 19:32:48 +0300

    fixed: type

--- application/org/core/collisionSolver.js
+++ application/org/core/collisionSolver.js
@@ -93,7 +93,7 @@
 	    	var collision = colliderTesters[c1][c2](f,s);
 		    if(collision !== false){
 		    	//console.log('{'+collision.normal.x+','+collision.normal.y+'}, '+collision.penetration);
-		    	o[0].emit('collision',{other: o[1], collsion: collision});
+		    	o[0].emit('collision',{other: o[1], collision: collision});
 		    	o[1].emit('collision',{other: o[0], collision: collision});
 		    }
 		    f.x -= o[0].x;
